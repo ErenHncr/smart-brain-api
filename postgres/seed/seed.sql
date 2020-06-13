e6e99c12ae8553f858941814411f1db5ce651dda
erenhncr <erenhncr@gmail.com>
2020-06-13 05:18:30 +0300

    new columns inserted

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (name,email,entries, joined) values ('Eren','a','5','2018-01-01');
+INSERT INTO users (name,email,entries, joined, pet,age) values ('Eren','a','5','2018-01-01','cat','18');
 INSERT INTO login (hash,email) values ('$2a$10$mY16T2zxColsoIalXYLCl.9D8hyDWXeLOcvMmrXZ4xLq4JFmBMpUC','a');
 
 COMMIT;
