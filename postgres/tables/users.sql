97e93b0aba80eee10aeac7a8244b8cf2f4d3040c
erenhncr <erenhncr@gmail.com>
2020-06-13 05:17:31 +0300

    new areas added

--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -5,7 +5,9 @@ create table users (
     name varchar(100),
     email text unique not null ,
     entries bigint default 0,
-    joined timestamp not null 
+    joined timestamp not null,
+    pet text default 'unknown',
+    age int default 18
 );
 
 COMMIT;
